48c83d99c9f04c2f5b3c3a875c1508aa41df462d
Patricl Pollet <ppollet@ppollet-laptop.(none)>
2011-04-27 16:30:56 +0200

    presque fini manque serialisation objets simples dans GsonUtils

---
+++ src/net/patrickpollet/moodlews_gson/core/Mdl_restserverBindingStub.java
@@ -0,0 +1,16 @@
+package net.patrickpollet.moodlews_gson.core;
+
+/**
+ * juste a naming class nothing new from whata has been retrieved from the WSDL
+ * @author ppollet
+ *
+ */
+public class Mdl_restserverBindingStub extends Mdl_soapserverBindingStub{
+
+	public Mdl_restserverBindingStub(String service_url, String nameSpace,
+			boolean debug) {
+		super(service_url, nameSpace, debug);
+		
+	}
+
+}
